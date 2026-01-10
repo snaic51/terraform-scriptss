d6e80ba1671441f3454d03a9143f0875ff8535b1
Stephen <srethinam1@dtcc.com>
2026-01-10 14:34:16 +0530

    updatedcode

--- provider.tf
+++ provider.tf
@@ -13,8 +13,4 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = "21989a2b-d981-49bc-b2da-28d16144c2cb"
-  client_secret   = "iuQ8Q~fOJLC1yaIbpywGIiBzZiCeltnBH917tbkb"
-  tenant_id       = "cabf5d06-7e3a-4d14-b7f4-2b5819ed881e"
-  subscription_id = "2df30ff1-915d-4d35-974a-3d3155aaa413"
 }
